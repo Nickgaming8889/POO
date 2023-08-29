471ca8110c7d425f0ee696ed75df568a0b8af430
Nickgaming8889 <92138380+Nickgaming8889@users.noreply.github.com>
2023-08-29 00:56:36 -0600

    29/08/2023

---
+++ MyClass2/Car.h
@@ -0,0 +1,19 @@
+#ifndef CAR_H
+#define CAR_H
+
+#include <iostream>
+using namespace std;
+
+
+class Car
+{
+
+    public:
+    //Atributos...
+
+    //Metodos...
+
+};
+
+
+#endif
---
+++ MyClass3/Person.h
@@ -0,0 +1,16 @@
+#ifndef PERSON_H
+#define PERSON_H
+
+#include <iostream>
+using namespace std;
+
+class Person {
+
+    public:
+    //Atributos...
+
+    //Metodos...
+
+};
+
+#endif
