0cb113c07e9a219c641bdd64ab6958a49e180228
Nickgaming8889 <92138380+Nickgaming8889@users.noreply.github.com>
2023-09-11 22:02:34 -0600

    11/09/2023

---
+++ Homework/Testing/Test1.h
@@ -0,0 +1,21 @@
+#ifndef TEST1_H
+#define TEST1_H
+
+#include <iostream>
+
+class Test1 {
+
+    private:
+
+    public:
+        //Attributes...
+        int hora, min, s;
+
+        //Metodos... 
+        Test1(/* args */);
+
+        ~Test1();
+};
+
+
+#endif
