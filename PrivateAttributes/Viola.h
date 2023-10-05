f543231110f322afce36ec9d827771dec664c8c3
Nickgaming8889 <92138380+Nickgaming8889@users.noreply.github.com>
2023-10-05 00:54:49 -0600

    05/10/2023 00:55

--- PrivateAttributes/Viola.h
+++ PrivateAttributes/Viola.h
@@ -11,7 +11,7 @@ class Viola {
         std::string marca;
         std::string afinacion;
         int numStrings;
-        Person* volume;
+        //Person* volume;
 
     public:
         //Metodos...
